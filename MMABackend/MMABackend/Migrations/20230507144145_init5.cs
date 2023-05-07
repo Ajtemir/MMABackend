300bb5507da9e007f81423b184b9f4a25a0712aa
Aytemir Ulanbek uulu <aytush2001@gmail.com>
2023-05-07 20:50:58 +0600

    temp

---
+++ MMABackend/MMABackend/Migrations/20230507144145_init5.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMABackend.Migrations
+{
+    public partial class init5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconId",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IconId",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
