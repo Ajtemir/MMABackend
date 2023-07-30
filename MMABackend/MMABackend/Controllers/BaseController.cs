8ae6a2df21f7e8391afc637232349ea2a6dc44a1
aulanbekuulu <aulanbekuulu@srs.lan>
2023-07-30 18:51:02 +0600

    added base controller

---
+++ MMABackend/MMABackend/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MMABackend.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private readonly ILogger<BaseController> _logger;
+
+        public BaseController(ILogger<BaseController> logger)
+        {
+            _logger = logger;
+        }
+        protected ActionResult Execute(Func<ActionResult> func)
+        {
+            try
+            {
+                return func.Invoke();
+            }
+            catch (ApplicationException e)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+    
+}
