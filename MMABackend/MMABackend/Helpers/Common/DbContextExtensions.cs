20f6565241da780b4baae51bc185be83c78502aa
aulanbekuulu <aulanbekuulu@srs.lan>
2023-08-12 12:35:55 +0600

    fix

--- MMABackend/MMABackend/Helpers/Common/DbContextExtensions.cs
+++ MMABackend/MMABackend/Helpers/Common/DbContextExtensions.cs
@@ -12,19 +12,19 @@ namespace MMABackend.Helpers.Common
         public static User GetUserByEmailOrError(this UnitOfWork uow, string email)
         {
             return uow.Users.FirstOrDefault(x => x.Email == email) ??
-                   throw new Exception("User not found by email");
+                   throw new ApplicationException("User not found by email");
         }
         
         public static string GetUserIdByEmailOrError(this UnitOfWork uow, string email)
         {
             return uow.Users.FirstOrDefault(x => x.Email == email)?.Id ??
-                   throw new Exception("User not found by email");
+                   throw new ApplicationException("User not found by email");
         }
         
         public static T FirstOrError<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, string errorMessage = null) where T: class
         {
             var elem = source.FirstOrDefault(predicate);
-            if(elem == null) throw new Exception(errorMessage ?? $"Not found entity {nameof(T)}");
+            if(elem == null) throw new ApplicationException(errorMessage ?? $"Not found entity {nameof(T)}");
             return elem;
         }
     }
