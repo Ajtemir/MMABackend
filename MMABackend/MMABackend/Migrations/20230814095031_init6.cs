40b8402ece7168bd27106a7f30701e57e4600536
aulanbekuulu <aulanbekuulu@srs.lan>
2023-08-14 17:29:41 +0600

    migration

---
+++ MMABackend/MMABackend/Migrations/20230814095031_init6.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMABackend.Migrations
+{
+    public partial class init6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Products");
+        }
+    }
+}
