4a86cf3fc2666e53a04eaf2a474b6ee2a62128d5
aulanbekuulu <aulanbekuulu@srs.lan>
2023-08-22 13:59:53 +0600

    reformat

--- MMABackend/MMABackend/DomainModels/Common/Market.cs
+++ MMABackend/MMABackend/DomainModels/Common/Market.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace MMABackend.DomainModels.Common
 {
     public class Market
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Shop> Shops { get; set; } = new List<Shop>();
     }
 }
--- MMABackend/MMABackend/DomainModels/Common/MarketShop.cs
+++ MMABackend/MMABackend/DomainModels/Common/MarketShop.cs
@@ -6,7 +6,7 @@ namespace MMABackend.DomainModels.Common
     public class MarketShop
     {
         public int Id { get; set; }
-        public int? MarketId { get; set; }
+        public int MarketId { get; set; }
         public Market Market { get; set; }
         public int ShopId { get; set; }
         public Shop Shop { get; set; }
