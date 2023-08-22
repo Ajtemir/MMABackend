36d61b18a4a48e5bfbcfa53fcb6642f77ce3bcac
Aytemir Ulanbek uulu <aytush2001@gmail.com>
2023-08-23 01:30:51 +0600

    refactor

--- MMABackend/MMABackend/Migrations/20230822192649_init10.Designer.cs
+++ MMABackend/MMABackend/Migrations/20230822192649_init10.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MMABackend.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MMABackend.Migrations
 {
     [DbContext(typeof(UnitOfWork))]
-    partial class UnitOfWorkModelSnapshot : ModelSnapshot
+    [Migration("20230822192649_init10")]
+    partial class init10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MMABackend/MMABackend/Migrations/20230822192649_init10.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMABackend.Migrations
+{
+    public partial class init10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shops_AspNetUsers_UserId1",
+                table: "Shops");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Shops_UserId1",
+                table: "Shops");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Shops");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Shops",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shops_UserId",
+                table: "Shops",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shops_AspNetUsers_UserId",
+                table: "Shops",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shops_AspNetUsers_UserId",
+                table: "Shops");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Shops_UserId",
+                table: "Shops");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Shops",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Shops",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shops_UserId1",
+                table: "Shops",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shops_AspNetUsers_UserId1",
+                table: "Shops",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
