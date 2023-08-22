a1ce0d563f07cc6a074adf22ae06bcba24147121
aulanbekuulu <aulanbekuulu@srs.lan>
2023-08-22 13:13:33 +0600

    добавил тип магазина

---
+++ MMABackend/MMABackend/Enums/Common/ShopType.cs
@@ -0,0 +1,29 @@
+﻿namespace MMABackend.Enums.Common
+{
+    /// <summary>
+    /// Тип магазина
+    /// </summary>
+    public enum ShopType
+    {
+        /// <summary>
+        /// Онлайн
+        /// Будет null в координатах
+        /// </summary>
+        Online = 1,
+        /// <summary>
+        /// Типа бутик
+        /// будет локация в городе
+        /// </summary>
+        Fixed = 2,
+        /// <summary>
+        /// Стихийная торговля
+        /// координаты в городе
+        /// </summary>
+        Free = 3,
+        /// <summary>
+        /// Внутри базара либо торгового центра
+        /// подробно расписать их расположение
+        /// </summary>
+        Market = 4,
+    }
+}
