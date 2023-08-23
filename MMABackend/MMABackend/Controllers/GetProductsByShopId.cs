94bc9a99cb93f6415c6233942d60426f778e307a
aulanbekuulu <aulanbekuulu@srs.lan>
2023-08-23 13:57:02 +0600

    action get products by shop id

---
+++ MMABackend/MMABackend/Controllers/GetProductsByShopId.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MMABackend.Helpers.Common;
+using MMABackend.ViewModelResults.Common;
+
+namespace MMABackend.Controllers
+{
+    public partial class ProductsController
+    {
+        [HttpGet("{shopId:int}")]
+        public ActionResult<GetProductByIdResult> GetProductsByShopId([FromQuery] GetProductsByShopId arguments, int shopId) => Execute(
+            () =>
+            {
+                var products = _uow.Shops.Include(x => x.Products)
+                    .FirstOrError(x => x.Id == shopId).Products.ToList();
+                return products;
+            });
+    }
+
+    public class GetProductsByShopId
+    {
+        
+    }
+}
