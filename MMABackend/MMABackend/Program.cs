62a38dd34ee9f4f2dd8592e88819d31259d2db72
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-24 14:14:47 +0600

    adding worker service which deletes expired collective

--- MMABackend/MMABackend/Program.cs
+++ MMABackend/MMABackend/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MMABackend
 {
@@ -18,6 +13,12 @@ namespace MMABackend
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>().UseIISIntegration(); });
     }
+
+    
 }
