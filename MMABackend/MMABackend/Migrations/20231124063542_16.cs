f3d71c428d952af5164047f60739ae8a88d9d928
aulanbekuulu <aulanbekuulu@srs.lan>
2023-11-24 12:38:13 +0600

    migration

---
+++ MMABackend/MMABackend/Migrations/20231124063542_16.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMABackend.Migrations
+{
+    public partial class _16 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "AuctionProductUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuctionElseReduction",
+                table: "AuctionProducts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "AuctionProductUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsAuctionElseReduction",
+                table: "AuctionProducts");
+        }
+    }
+}
