7b7a6dea93953c3fb1a1a19c95e69f20ae4f8d5e
Aytemir Ulanbek uulu <aytush2001@gmail.com>
2024-02-24 17:37:18 +0600

    search worked

--- MMABackend/MMABackend/Controllers/SearchProducts.cs
+++ MMABackend/MMABackend/Controllers/SearchProducts.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MMABackend.DomainModels.Common;
 
 namespace MMABackend.Controllers
 {
@@ -11,24 +13,44 @@ namespace MMABackend.Controllers
         [HttpPost]
         public ActionResult Search([FromBody]SearchArgument argument) =>  Execute(() =>
         {
-            var dict = argument.PropertyFilters.GroupBy(x => x.IsMultiOrNumberDefault).ToDictionary(x=>x.Key, x=>x.ToList());
-            var singleAndMultiValues = dict[false].Select(x => x.SingleValues).ToList();
-            dict[true].ForEach(x => singleAndMultiValues.AddRange(x.MultiValues));
+            var singleAndMultiValues = new List<int>();
+            foreach (var multiFilter in argument.PropertyFilters.Where(x=>x.IsMultiOrNumberDefault != null && x.IsMultiOrNumberDefault.Value))
+            {
+                singleAndMultiValues.AddRange(multiFilter.MultiValues);
+            }
+            foreach (var multiFilter in argument.PropertyFilters.Where(x=>x.IsMultiOrNumberDefault != null && !x.IsMultiOrNumberDefault.Value))
+            {
+                singleAndMultiValues.Add(multiFilter.SingleValues);
+            }
             
-            var products = _uow.Products
-                .Include(x => 
-                    x.ProductProperties.Where(p => singleAndMultiValues.Contains(p.PropertyValueId.Value))
-                        .Where(p => dict[null].Any(nv => 
-                            p.PropertyKeyId == nv.PropertyId && nv.StartValue.Value <= p.NumberValue && p.NumberValue <= nv.EndValue.Value )
-                        )
-                )
+            var productsFilteredByMainData = _uow.Products
+                .Include(x=>x.ProductProperties)
                 .Where(x =>
                     (argument.Description == null || x.Description.Contains(argument.Description)) &&
                     ((argument.StartPrice == null && argument.EndPrice == null) || x.Price == null || argument.StartPrice <= x.Price && x.Price <= argument.EndPrice) &&
                     (argument.CategoryId == null || x.CategoryId == argument.CategoryId)
-                )
-                .ToList();
-             return products;
+                ).ToList();
+
+            if (singleAndMultiValues.Any())
+            {
+                productsFilteredByMainData = productsFilteredByMainData
+                    .Where(x => x.ProductProperties.Select(z => z.PropertyValueId ?? 0).Any(y => singleAndMultiValues.Contains(y)))
+                    .ToList();
+            }
+            
+
+            var rangeProperties = argument.PropertyFilters.Where(x => x.IsMultiOrNumberDefault == null).ToList();
+            if (rangeProperties.Any())
+            {
+                productsFilteredByMainData = productsFilteredByMainData.Where(p => p.ProductProperties.Any(pp =>
+                    rangeProperties
+                        .Any(rp => pp.PropertyKeyId == rp.PropertyId &&
+                                   (rp.StartValue == null || rp.StartValue <= pp.NumberValue) &&
+                                   (rp.EndValue == null || pp.NumberValue <= rp.EndValue))
+                )).ToList();
+            }
+            
+            return productsFilteredByMainData;
         });
     }
 
@@ -38,7 +60,7 @@ namespace MMABackend.Controllers
         public decimal? StartPrice { get; set; }
         public decimal? EndPrice { get; set; }
         public int? CategoryId { get; set; }
-        public PropertyFilter[] PropertyFilters { get; set; }
+        public PropertyFilter[] PropertyFilters { get; set; } = Array.Empty<PropertyFilter>();
     }
 
     public class PropertyFilter
