bd8c48bfaca76abc2337f4aa25467c0174bbbd8d
aulanbekuulu <aulanbekuulu@srs.lan>
2024-04-19 20:31:17 +0600

    work

--- MMABackend/MMABackend/Controllers/UpdateProperties.cs
+++ MMABackend/MMABackend/Controllers/UpdateProperties.cs
@@ -11,23 +11,48 @@ namespace MMABackend.Controllers
         [HttpPost("{productId:int}")]
         public ActionResult UpdateProperties(int productId, [FromBody] UpdatePropertiesArgument argument) => Execute(() =>
         {
-            // _uow.RemoveWhere<ProductProperty>(x=>x.ProductId == productId);
+            var existProperties = _uow.ProductProperties.Where(x => x.ProductId == productId).ToList();
+            _uow.RemoveRange(existProperties);
+            _uow.SaveChanges();
+            var productProperties = new List<ProductProperty>();
             foreach (var property in argument.Properties)
             {
                 switch (property.IsMultipleOrLiteralDefault)
                 {
                     case true:
+                        productProperties.AddRange(property.CurrentMultiValues.Select(x=> new ProductProperty
+                        {
+                            ProductId = productId,
+                            PropertyKeyId = property.Id,
+                            PropertyValueId = x,
+                        }));
                         break;
                     case false:
                         if (property.CurrentSingleValue != null)
                         {
-                            
+                            productProperties.Add(new ProductProperty
+                            {
+                                ProductId = productId,
+                                PropertyKeyId = property.Id,
+                                PropertyValueId = property.CurrentSingleValue,
+                            });
                         }
                         break;
                     case null:
+                        if (property.CurrentNumberValue != null)
+                        {
+                            productProperties.Add(new ProductProperty
+                            {
+                                ProductId = productId,
+                                PropertyKeyId = property.Id,
+                                NumberValue = property.CurrentNumberValue,
+                            });
+                        }
                         break;
                 }
             }
+            _uow.AddRange(productProperties);
+            _uow.SaveChanges();
         });
     }
 
